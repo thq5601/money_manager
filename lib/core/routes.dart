ad9d7c7edfe42fb1052d13b8808bc715cc9fe160
QuanTH <thq5601@gmail.com>
2025-07-23 16:02:17 +0700

    Add budget planning route and screen to AppRoutes for enhanced financial management features

--- lib/core/routes.dart
+++ lib/core/routes.dart
@@ -4,6 +4,7 @@ import 'package:money_manager/features/account_manager/sign_up/sign_up_screen.da
 import 'package:money_manager/features/home/home_screen.dart';
 import 'package:money_manager/features/profile/profile.dart';
 import 'package:money_manager/features/splash/splash_screen.dart';
+import 'package:money_manager/features/analytics/budget_planning_screen.dart';
 
 class AppRoutes {
   // Route names
@@ -13,6 +14,7 @@ class AppRoutes {
   static const String home = '/home';
   static const String profile = '/profile';
   static const String transactions = '/transactions';
+  static const String budgetPlanning = '/budget-planning';
 
   // Route map for MaterialApp
   static Map<String, WidgetBuilder> get routes => {
@@ -21,6 +23,7 @@ class AppRoutes {
     signup: (context) => const SignUpScreen(),
     home: (context) => const HomeScreen(),
     profile: (context) => const ProfileScreen(),
+    budgetPlanning: (context) => const BudgetPlanningScreen(),
   };
 
   // Route generator for dynamic routes
@@ -39,6 +42,10 @@ class AppRoutes {
       case transactions:
         // If you want to support deep linking to the transactions tab, handle it in HomeScreen
         return MaterialPageRoute(builder: (context) => const HomeScreen());
+      case budgetPlanning:
+        return MaterialPageRoute(
+          builder: (context) => const BudgetPlanningScreen(),
+        );
       default:
         // Return splash screen for unknown routes
         return MaterialPageRoute(builder: (context) => const SplashScreen());
